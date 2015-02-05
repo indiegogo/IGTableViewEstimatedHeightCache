96c316a015937003f1e0644fe2750837b70dfdcf
Glen Tregoning <glen.tregoning@indiegogo.com>
2015-02-04 20:45:06 -0800

    Adding Example Pods files

---
+++ Example/Pods/Target Support Files/Pods-IGTableViewEstimatedHeightCache-IGTableViewEstimatedHeightCache/Pods-IGTableViewEstimatedHeightCache-IGTableViewEstimatedHeightCache-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_IGTableViewEstimatedHeightCache_IGTableViewEstimatedHeightCache : NSObject
+@end
+@implementation PodsDummy_Pods_IGTableViewEstimatedHeightCache_IGTableViewEstimatedHeightCache
+@end
---
+++ Example/Pods/Target Support Files/Pods-Tests/Pods-Tests-environment.h
@@ -0,0 +1,20 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// IGTableViewEstimatedHeightCache
+#define COCOAPODS_POD_AVAILABLE_IGTableViewEstimatedHeightCache
+#define COCOAPODS_VERSION_MAJOR_IGTableViewEstimatedHeightCache 0
+#define COCOAPODS_VERSION_MINOR_IGTableViewEstimatedHeightCache 2
+#define COCOAPODS_VERSION_PATCH_IGTableViewEstimatedHeightCache 0
+
+// Kiwi
+#define COCOAPODS_POD_AVAILABLE_Kiwi
+#define COCOAPODS_VERSION_MAJOR_Kiwi 2
+#define COCOAPODS_VERSION_MINOR_Kiwi 3
+#define COCOAPODS_VERSION_PATCH_Kiwi 1
+
